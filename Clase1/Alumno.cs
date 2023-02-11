ece09ddd66098dcc572234d91e9cc940ddc6ad8b
Jesus Farias <jesus.fg2630@gmail.com>
2023-02-10 23:08:39 -0300

    inicial

---
+++ Clase1/Alumno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    internal class Alumno
+    {
+        //Campos || Atributos
+        public int Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Apellido { get; set; }
+
+        public string TraerDatos() 
+        {
+            string datos = $"Datos: {Dni} {Nombre} {Apellido} {Email}";
+            return datos;
+        } 
+    }
+}
---
+++ Clase1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Clase1
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            //Primer Instancia
+            Alumno miAlumno = new Alumno();
+
+            miAlumno.Nombre = "Jesus";
+            miAlumno.Apellido = "Farias";
+            miAlumno.Dni = 8472932;
+            miAlumno.Email = "jesus.fg2630@gmail.com";
+
+            //Segunda Instancia
+            Alumno miAlumno2 = new Alumno();
+
+            miAlumno2.Nombre = "Lucas";
+            miAlumno2.Apellido = "Perez";
+            miAlumno2.Dni = 12354123;
+            miAlumno2.Email = "Perelu@gmail.com";
+
+            Console.WriteLine(miAlumno.TraerDatos());
+            Console.WriteLine(miAlumno2.TraerDatos());
+            
+
+        }
+
+    }
+}
